6519a760b6ecd8e5670197bc75834eec44ebee1a
Amie Croteau <ac04400@ravens.racc.edu>
2020-05-26 18:37:54 -0400

    Scaffold started

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.tealAccent,
+          appBar: AppBar(
+            title: Text('Magic 8 Ball'),
+            backgroundColor: Colors.teal,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/ball1.png'),
+            )
+          )
+        ],
+      ),
+    );
+  }
+};
